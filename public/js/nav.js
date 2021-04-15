7d57f53fd5db637dbbad3cb16c36a9c194b65856
JayTheMate <julenius.jesse@gmail.com>
2021-04-15 14:09:47 +0300

    Added mobile nav button, nav alignments...

---
+++ public/js/nav.js
@@ -0,0 +1,25 @@
+'use strict';
+// redirect to https protocol if needed
+if (window.location.protocol == 'http:') {
+  console.log("Redirect to https protocol");
+  window.location.href =
+      window.location.href.replace('http:', 'https:');
+}
+
+// get close and open button elements
+let openBtn = document.getElementById("openBtn");
+let sidenav = document.getElementById("sidenav");
+
+// add event listeners
+openBtn.addEventListener("click", open);
+
+// function to handle navbar opening
+function open() {
+  if (sidenav.style.width == '223px') {
+    sidenav.style.width = '0';
+    sidenav.style.opacity = 0;
+  } else {
+    sidenav.style.width = "223px";
+    sidenav.style.opacity = 1.5;
+  }
+}
