2660c22a9f3aae0e578e1f051d1d67dc5c95e80b
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-16 19:35:48 +0300

    Changed port for production centos server

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const userRoute = require('./routes/userRoute');
 const passport = require('./utils/pass');
 const authRoute = require('./routes/authRoute');
 const app = express();
-const port = process.env.HTTP_PORT || 3000;
+const port = process.env.HTTP_PORT || 3001;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 if (process.env.NODE_ENV === 'production') {
   require('./utils/production')(app, port);
 } else {
-  require('./utils/localhost')(app, process.env.HTTPS_PORT || 8000, port);
+  require('./utils/localhost')(app, process.env.HTTPS_PORT || 8001, port);
 }
 app.get('/', (req, res) => {
   res.send('Hello Secure World!');
