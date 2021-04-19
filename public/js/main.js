7948409b46056f321d31d9694501a737032aefbf
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-19 20:23:45 +0300

    Fixed showing login and registration form. Form is changed with javascript. Logout button will be shown when user has logged in

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,32 @@
+'use strict';
 const url = 'https://localhost:8001'; // change url when uploading to server
+const loginNow = document.querySelector('#login-now');
 const loginForm = document.querySelector('#login-form');
+const logOut = document.querySelector('#log-out');
+const registerNow = document.querySelector('#register-now');
+const registerForm = document.querySelector('#register-form');
+
+// when app starts, check if token exists and hide login form, show logout button and main content, get cats and users
+if (sessionStorage.getItem('token')) {
+    loginForm.style.display = 'none';
+    logOut.style.display = 'block';
+} else {
+    logOut.style.display = 'none';
+    loginForm.style.display = 'flex';
+}
+
+registerNow.addEventListener('click', showRegistration);
+loginNow.addEventListener('click', showLogin);
+
+function showLogin() {
+    loginForm.style.display = 'flex';
+    registerForm.style.display = 'none';
+}
+
+function showRegistration() {
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'flex';
+}
 
 // login
 loginForm.addEventListener('submit', async (evt) => {
@@ -21,6 +48,32 @@ loginForm.addEventListener('submit', async (evt) => {
     } else {
       // save token
       sessionStorage.setItem('token', json.token);
+      loginForm.style.display = 'none';
       window.location.href = url + "/forum.html";
     }
-  });
+});
+
+// logout
+logOut.addEventListener('click', async (evt) => {
+    evt.preventDefault();
+    try {
+      const options = {
+        headers: {
+          'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+        },
+      };
+      const response = await fetch(url + '/auth/logout', options);
+      const json = await response.json();
+      console.log(json);
+      // remove token
+      sessionStorage.removeItem('token');
+      alert('You have logged out');
+      // show/hide login form and logout
+      loginForm.style.display = 'block';
+      logOut.style.display = 'none';
+      window.location.href = 'index.html';
+    }
+    catch (e) {
+      console.log(e.message);
+    }
+});
