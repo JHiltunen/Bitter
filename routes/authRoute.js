4f57dd4e98773a807385c6a9ff28b83f2773f3ac
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-20 13:50:23 +0300

    Fixed date comparison on authRoute

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,7 +4,7 @@ const { body, check } = require('express-validator');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const currentDate = new Date();
-let formatted_date = currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + currentDate.getDate();
+
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
@@ -13,17 +13,14 @@ router.post('/register',
       body('firstname', 'minimum 3 characters').isLength({min: 3}),
       body('lastname', 'minimum 3 characters').isLength({min: 3}),
       body('username', 'email is not valid').isEmail(),
-      /*check('dateOfBirth').custom((dateOfBirth, {req}) => {
-        console.log('Birthdate: ' + dateOfBirth);
-        console.log('Current date: ' + formatted_date);
-        console.log('Current date later: ' + currentDate.toISOString().split('T')[0]);
-        //console.log('Formatted: ' + dateFormat.format(cur))
-        if (body('dateOfBirth') < currentDate.toISOString().split('T')[0]) {
+      check('dateOfBirth').custom((dateOfBirth, {req}) => {
+        const birthday = new Date(dateOfBirth);
+        if (birthday > currentDate) {
           throw new Error('Birthdate can not be in the future');
         } else {
           return true;
         }
-      }),*/
+      }),
       // TODO. REGEX for gender enum
       body('password', 'at least one upper case letter').
           matches('(?=.*[A-Z]).{8,}'),
