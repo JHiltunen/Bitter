3b592d143d3803a89362c7283e4627328ba04b5a
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-23 15:09:20 +0300

    Added winston

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-
+const winston = require('winston');
 const cors = require('cors');
 const userRoute = require('./routes/userRoute');
 const adminRoute = require('./routes/adminRoute');
@@ -16,6 +16,30 @@ if (process.env.NODE_ENV === 'production') {
   require('./utils/production')(app, port);
 } else {
   require('./utils/localhost')(app, process.env.HTTPS_PORT || 8001, port);
+}
+
+// logging for server
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
 }
 
 // root directory
