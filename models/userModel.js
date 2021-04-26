17cc563acf62e9d66fa94f079065cfa9f9675556
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-26 11:42:12 +0300

    Added logging for userModel

--- models/userModel.js
+++ models/userModel.js
@@ -9,26 +9,39 @@ const getUser = async (id) => {
     const [rows] = await promisePool.execute('SELECT * FROM users INNER JOIN user_roles ON users.userId = user_roles.userId INNER JOIN roles ON user_roles.roleId = roles.roleId WHERE users.userId = ?', [id]);
     return rows[0];
   } catch (e) {
+    logger.error(`Error while fetching database: ${e}`);
     console.error('userModel:', e.message);
   }
 };
 
 const insertUser = async (user) => {
-  const [row] = await promisePool.execute('INSERT INTO users (firstname, lastname, email, dateOfBirth, gender, password, vst) VALUES (?, ?, ?, ?, ?, ?, curdate())', [user.firstname, user.lastname, user.username, user.dateOfBirth, user.gender, user.password]);
-  console.log('insert row', row);
-  await giveUserDefaultRole(row.insertId);
-  return row.insertId;
+  try {
+    const [row] = await promisePool.execute('INSERT INTO users (firstname, lastname, email, dateOfBirth, gender, password, vst) VALUES (?, ?, ?, ?, ?, ?, curdate())', [user.firstname, user.lastname, user.username, user.dateOfBirth, user.gender, user.password]);
+    console.log('insert row', row);
+    await giveUserDefaultRole(row.insertId);
+    return row.insertId;
+  } catch (e) {
+    logger.error(`Error while fetching database: ${e}`);
+  }
 };
 
 const giveUserDefaultRole = async (id) => {
-  const [row] = await promisePool.execute('INSERT INTO user_roles (roleId, userId) VALUES (2, ?)', [id]);
-  return row.insertId;
+  try {
+    const [row] = await promisePool.execute('INSERT INTO user_roles (roleId, userId) VALUES (2, ?)', [id]);
+    return row.insertId;
+  } catch (e) {
+    logger.error(`Error while fetching database: ${e}`);
+  }
 }
 
 const updateUser = async (user) => {
-  const [row] = await promisePool.execute('UPDATE users (name, email) VALUES (?, ?)', [user.name, user.username]);
-  console.log('insert row', row);
-  return row.insertId;
+  try {
+    const [row] = await promisePool.execute('UPDATE users (name, email) VALUES (?, ?)', [user.name, user.username]);
+    console.log('insert row', row);
+    return row.insertId;
+  } catch (e) {
+    logger.error(`Error while fetching database: ${e}`);
+  }
 };
 
 const getUserLogin = async (params) => {
@@ -39,6 +52,7 @@ const getUserLogin = async (params) => {
         params);
     return rows;
   } catch (e) {
+    logger.error(`Error while fetching database: ${e}`);
     console.log('error', e.message);
   }
 };
