bcea742170281282675f036b97c3d9af8ec2f7f0
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-28 16:37:05 +0300

    Fixed issue with where contact form send to database didn't save time

--- models/contactModel.js
+++ models/contactModel.js
@@ -6,11 +6,10 @@ const promisePool = pool.promise();
 const insertContact = async (contact) => {
   try {
     logger.info(`Prepare for database ${contact}`);
-    const [rows] = await promisePool.execute('INSERT INTO contacts (name, email, message, hasBeenRead, vst) VALUES(?, ?, ?, \'false\', curdate())', contact);
+    const [rows] = await promisePool.execute('INSERT INTO contacts (name, email, message, hasBeenRead, vst) VALUES(?, ?, ?, \'false\', now())', contact);
     logger.info(`Insert row on insertContact: ${JSON.stringify(rows)}`);
     return rows;
   } catch (e) {
-    console.error('contactModel -> Insert contact:', e.message);
     logger.error(`Error while inserting to database ${e}`);
   }
 };
