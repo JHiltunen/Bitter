e7102dd0036de69bdb9f69e56336d9120bca7b81
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-05-06 14:43:36 +0300

    Changed admin route

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,6 +4,11 @@ const { body } = require('express-validator');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
   
+router.route('/')
+  .get((req, res) => {
+    res.sendFile('admin.html');
+  })
+
 router.route('/users/')  
   .get(adminController.user_list_get);
 
