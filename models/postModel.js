929292ff7e0d50b438dc4b0db12f800ee5ffbeed
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-05-07 11:43:21 +0300

    Fixed missing columns on database query

--- models/postModel.js
+++ models/postModel.js
@@ -48,7 +48,7 @@ const createPost = async (post) => {
     console.log('Row 72:', post);
     logger.info(`createPost post: ${JSON.stringify(post)}`);
     const [rows] = await promisePool.execute(
-        'INSERT INTO posts (title, content, image, likes, dislikes, coords, userId, vst) VALUES (?, ?, ?, 0, 0, ?, ?, now())', post);
+        'INSERT INTO posts (title, content, image, coords, userId, vst) VALUES (?, ?, ?, ?, ?, now())', post);
     logger.info(`createPost : ${JSON.stringify(rows)}`);
     return rows;
   } catch (e) {
